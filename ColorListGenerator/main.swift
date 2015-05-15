1bb6ed0baa4614c4ebc1d79d9dfe2545d46aeeb1
griffin_stewie <panterathefamilyguy@gmail.com>
2015-05-15 21:12:10 +0900

    Initial Commit

---
+++ ColorListGenerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ColorListGenerator
+//
+//  Created by Zushi Tatsuya on 2015/05/15.
+//  Copyright (c) 2015年 net.cyan-stivy. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
