c5003b9847ce3970dc6162b258a51a003692e514
griffin-stewie <panterathefamilyguy@gmail.com>
2020-03-23 19:24:50 +0900

    code subcommand now supports various input clg supports

--- Sources/clg/Command/Subcommands/CodeCommand.swift
+++ Sources/clg/Command/Subcommands/CodeCommand.swift
@@ -15,27 +15,25 @@ extension Code: ExpressibleByArgument { }
 struct CodeCommand: ParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "code",
-        abstract: "generates Swift code, Objective-C code, Color Set, colors.xml from JSON"
+        abstract: "generates Swift code, Objective-C code, Color Set, colors.xml from input"
     )
 
-    @Option(name: [.customLong("code"), .customShort("c")], help: ArgumentHelp("generate specific type of code file", valueName: Code.allCasesDescription))
+    @Option(name: [.customLong("code"), .customShort("c")], help: ArgumentHelp("Generate specific type of code file", valueName: Code.allCasesDescription))
     var codeType: Code
 
     @Option(name: [.customLong("output"), .customShort("o")], default: Path.cwd/".", help: ArgumentHelp("Output directory that generated file will be saved. (default: current directory)", valueName: "directory"))
     var outputPath: Path
 
-    @Argument(help: ArgumentHelp("", valueName: "JSON file path"))
+    @Argument(help: ArgumentHelp("Supported file types are \(FileType.allCasesDescription)", valueName: "input file"))
     var inputFilePath: Path
 
     func run() throws {
-        let jsonData = try Data(contentsOf: inputFilePath.url)
+        let fileType = try FileType(from: inputFilePath.url)
 
-        guard let colorDicts = try JSONSerialization.jsonObject(with: jsonData, options: .allowFragments) as? [[String:String]] else {
-            throw RuntimeError("Invalid JSON file")
+        guard let colorList = fileType.colorListFrom(url: inputFilePath.url) else {
+            throw RuntimeError("Could not load \(inputFilePath.string)")
         }
 
-        let colors: [Color] = colorDicts.map{ Color(dictionary: $0) }
-
-        codeType.generateCode(colors, directory: outputPath.string)
+        codeType.generateCode(colorList.colors, directory: outputPath.string)
     }
 }
