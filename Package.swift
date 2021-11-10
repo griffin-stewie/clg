e2729377a871e161686f79dbcfa2577f102d8aca
griffin-stewie <panterathefamilyguy@gmail.com>
2021-11-10 23:40:23 +0900

    update Xcode

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
+        .executableTarget(
             name: "clg",
             dependencies: [
                 "Core",
